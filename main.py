4e3d99cb8d076b7a5409d662aa4da8cd635b878f
Bachynin Ivan <bachynin.i@gmail.com>
2018-07-23 03:51:52 +0300

    refactor

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import argparse
 import configparser
 import json
 import os
+from functools import wraps
 from pprint import pprint
 
 
-def set_name(m: dict, name: str):
-    if name != '':
-        m['name'] = 'theme'
-    return True
+def meta(msg, key):
+    def dec(f):
+        f.msg = msg
+        f.key = key
 
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            return f(*args, **kwargs)
 
-def set_logo(m: dict, value: str):
-    if value == 'white':
-        m['theme']['properties']['ntp_logo_alternate'] = 1
-    return True
+        return wrapper
+
+    return dec
 
 
 def convert_hex(value: str):
@@ -41,48 +44,16 @@ def get_rgb(value: str):
     return rgb
 
 
-def set_frame_color(m: dict, value: str):
-    try:
-        rgb = get_rgb(value)
-        m['frame'] = rgb
-        return True
-    except Exception as e:
-        print(e, '- hex or rgb are acceptable')
-        return False
-
-
-def set_bg_color(m: dict, value: str):
-    if not value:
-        value = list(map(lambda x: min(255, int(x * 1.1 + 20)), m['frame']))
-    else:
-        try:
-            value = get_rgb(value)
-        except Exception as e:
-            print(e, '- hex or rgb are acceptable')
-            return False
-    m['ntp_background'] = m['toolbar'] = value
-    return True
-
-
-def save_manifest(m: dict, path=None):
-    if not path:
-        path = input("manifest path/name >  ")
-        if not path:
-            path = '.'
-    if not os.path.exists(path):
-        os.makedirs(path)
-    path = os.path.join(path, 'manifest.json')
-    with open(path, 'w') as f:
-        json.dump(m, f, indent=4)
-
-
-def process_steps(sub_manifest, steps):
-    for info, func, value_from_config in steps:
+def process_steps(sub_manifest: dict, config_data: dict, steps: list):
+    for func in steps:
+        msg = func.msg
+        key = func.key
+        value_from_config = config_data.get(key, None)
         while True:
             if value_from_config:
                 value = value_from_config
             else:
-                value = input(info + ' >  ')
+                value = input(msg + ' >  ')
             ok = func(sub_manifest, value)
             if ok:
                 break
@@ -123,12 +94,63 @@ def read_config(config_fp):
     return dict(config['gen'])
 
 
+def save_manifest(m: dict, path=None):
+    if not path:
+        path = input("manifest path/name >  ")
+        if not path:
+            path = '.'
+    if not os.path.exists(path):
+        os.makedirs(path)
+    path = os.path.join(path, 'manifest.json')
+    with open(path, 'w') as f:
+        json.dump(m, f, indent=4)
+
+
+@meta("extension name (default 'theme')", 'name')
+def set_name(m: dict, name: str):
+    if name != '':
+        m['name'] = 'theme'
+    return True
+
+
+@meta("google logo (choose: color | white, default 'color')", 'logo')
+def set_logo(m: dict, value: str):
+    if value == 'white':
+        m['theme']['properties']['ntp_logo_alternate'] = 1
+    return True
+
+
+@meta("set frame color (hex or rgb)", 'frame-color')
+def set_frame_color(m: dict, value: str):
+    try:
+        rgb = get_rgb(value)
+        m['frame'] = rgb
+        return True
+    except Exception as e:
+        print(e, '- hex or rgb are acceptable')
+        return False
+
+
+@meta("set toolbar color (default is slightly lighter version of frame color)", 'toolbar-color')
+def set_toolbar_color(m: dict, value: str):
+    if not value:
+        value = list(map(lambda x: min(255, int(x * 1.1 + 20)), m['frame']))
+    else:
+        try:
+            value = get_rgb(value)
+        except Exception as e:
+            print(e, '- hex or rgb are acceptable')
+            return False
+    m['ntp_background'] = m['toolbar'] = value
+    return True
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('config', default=None, nargs='?')
     args = parser.parse_args()
     config_fp = args.config or 'config.ini'
-    data = read_config(config_fp)
+    config_data = read_config(config_fp)
 
     manifest = {
         'manifest_version': 2,
@@ -148,28 +170,21 @@ def main():
     colors = manifest['theme']['colors']
     tints = manifest['theme']['tints']
 
-    steps = [
-        ("name (default 'theme')",
-         set_name, data.get('name', None)),
-        ("google logo (default 'color', might be 'white')",
-         set_logo, data.get('logo', None)),
-    ]
-    process_steps(manifest, steps)
-
-    steps = [
-        ("set frame color (hex or rgb)",
-         set_frame_color, data.get('frame-color', None)),
-        ("set toolbar color (default is lighter frame color)",
-         set_bg_color, data.get('toolbar-color', None)),
-    ]
-    process_steps(colors, steps)
+    process_steps(manifest, config_data, [
+        set_name,
+        set_logo,
+    ])
+    process_steps(colors, config_data, [
+        set_frame_color,
+        set_toolbar_color,
+    ])
 
     light = is_light(colors)
     setup_colors(colors, light)
     setup_tints(tints, light)
 
     pprint(manifest)
-    save_manifest(manifest, path=data.get('save-path', None))
+    save_manifest(manifest, path=config_data.get('save-path', None))
 
 
 if __name__ == '__main__':
